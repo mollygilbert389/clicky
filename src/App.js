5d3a294f9472c62e74edca1a49e3e7677da02def
Molly Gilbert <msgilbert389@gmail.com>
2019-04-22 13:52:39 -0400

    adding lose alert

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     order: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     message: "Click a pic to play",
     show: true,
+    loseShow: false,
     friends
   };
 
@@ -32,6 +33,7 @@ class App extends Component {
         message: "You got it! Keep going."
       });
     } else {
+      alert("You've lost!")
       if (this.state.score > this.state.topscore) {
         this.setState({ topscore: this.state.score })
       }
@@ -57,7 +59,6 @@ class App extends Component {
   handleClose() {
     this.setState({ show: false });
   }
-  
 
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -71,6 +72,18 @@ class App extends Component {
               <Modal.Title>Fun fun game!</Modal.Title>
             </Modal.Header>
             <Modal.Body>Welcome to Molly's memory game! To win this game you must click on a different image till you've clicked all 12. You can watch your score in the top right corner. Good luck!</Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={this.handleClose = this.handleClose.bind(this)}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+           <Modal loseShow={this.state.show} onHide={this.state.hide}>
+            <Modal.Header>
+              <Modal.Title>You've Lost!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>You've lost!</Modal.Body>
             <Modal.Footer>
               <Button variant="primary" onClick={this.handleClose = this.handleClose.bind(this)}>
                 Close
